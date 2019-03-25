ffe14214855ee78c67f5160c0280d54167f66fdd
Dillon O'Kelley <dillonokelley@Dillons-MacBook-Air.local>
2019-03-25 13:07:51 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log('we connected')
+
+let count = 0
+let element = document.getElementById('counter')
+
+
+
+function increase(){
+    count++
+    element.innerText = count
+    console.log(count)
+}
+
+function decrease(){
+    count--
+    element.innerText = count
+    console.log(count)
+}
+
+function reset(){
+    count = 0
+    element.innerHTML = '<mark>' + count + '</mark>'
+    console.log(count)
+}
+
+function selectTheme(theme){
+    document.getElementsByTagName('body')[0].className = theme;
+    document.getElementsByTagName('main')[0].className = theme;
+    let button = document.getElementsByTagName('button')
+    for(let i = 0; i < button.length; i++){
+        button[i] = theme
+    }
+}
